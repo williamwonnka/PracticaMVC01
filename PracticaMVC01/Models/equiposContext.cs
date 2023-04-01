f58a3c775323cc1a3101a13f1d7b6665773415f6
William Emerson Cuellar Diaz <martin.sanabria@creativaconsultores.com>
2023-03-31 21:43:17 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC01/Models/equiposContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> dbContext) : base(dbContext)
+        {
+
+
+        }
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
